f5911641c4a2c6f7cfa566d7c0beff15260d6bad
Chinedu Abalogu <chineduabalogu@yahoo.com>
2016-06-10 14:02:15 +0100

    send mail

--- mail.php
+++ mail.php
@@ -1,14 +1,62 @@
 <?php
-require 'sendgrid-php/vendor/autoload.php'; //Not required with Composer
-$sendgrid = new SendGrid("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");
+require("PHPMailer/PHPMailerAutoload.php");
+$mail = new PHPMailer();
 
-$email = new SendGrid\Email();
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
+$mail->IsSMTP();
+$mail->SMTPAuth = true; // enable SMTP authentication
+$mail->SMTPSecure = "ssl"; 
+$mail->Host = "plus.smtp.mail.yahoo.com";
+$mail->Port = 465; // set the SMTP port
+$mail->Username = "chineduabalog@yahoo.com";
+$mail->Password = "pmsschool"; 
+$mail->From = $email;
+$mail->FromName = $name;
+$mail->AddAddress("info@bennyhotellagos.com");
 
-if(isset($_POST['submit'])){
+$mail->isHTML(true);
+
+$mail->Subject = "Contact form";
+$mail->Body = $message;
+
+// SEND TO YOURSELF
+
+$mail->addReplyTo( $email, $name );
+$mail->addAddress( 'info@bennyhotellagos.com', 'Benny Hotel' );
+$mail->send();
+
+// CLEAR REPLY TO AND RECIPIENTS 
+
+$mail->clearReplyTos();
+$mail->clearAllRecipients();
+
+// SEND TO THE USER
+
+$mail->addAddress( $email, $first_name );
+$mail->addReplyTo( 'your@email.com', 'You' );
+
+if(!$mail->Send())
+{
+    echo 'Message was not sent.';
+    echo 'Mailer error: ' . $mail->ErrorInfo;
+}
+else
+{
+    header("Location: /thankyou2.html");
+}
+?>
+<!-- if(isset($_POST['submit'])){
+    $mail->From = "sumthing@yahoo.com";
+    $mail->FromName = "myname";
+    $mail->AddAddress("you@example.com");
+    $mail->Subject = "Test PHPMailer Message";
+    $mail->Body = "Hi! \n\n This was sent with phpMailer_example3.php.";
+
+    
+
     $name = $_POST['name'];
     $email = $_POST['email_address'];
     $message = $_POST['message'];
@@ -22,70 +70,4 @@ if(isset($_POST['submit'])){
                 Message: $message <body></title></head></html>');
 
     $sendgrid->send($email);    
-    header("Location: /thankyou2.html");
-?>
-<!-- $message = array(
-    'subject' => 'Contact form message',
-    'from_email' => 'no-reply@bennyhotel.com',
-    'html' => $name . "\n email: " . $email_address . "\n wrote the following:" . "\n\n" . $mes ,
-    'to' => array(array('email' => 'info@bennyhotellagos.com', 'name' => 'Benny Hotel front desk')),
-    'merge_vars' => array(array(
-        'rcpt' => 'recipient1@domain.com',
-        'vars' =>
-        array(
-            array(
-                'name' => 'FIRSTNAME',
-                'content' => 'Recipient 1 first name'),
-            array(
-                'name' => 'LASTNAME',
-                'content' => 'Last name')
-        ))));
-
-    $sendgrid->send($message, $async=false, $ip_pool=null, $send_at=null);
-    } -->
-<!-- 
-// use actual sendgrid username and password in this section
-$url = 'https://api.sendgrid.com/'; 
-$user = 'mrabalogu'; // place SG username here
-$pass = '1Pmsschool'; // place SG password here
-// grabs HTML form's post data; if you customize the form.html parameters then you will need to reference their new new names here
-$name = $_POST['name']; 
-$email = $_POST['email_address']; 
-$message = $_POST['message'];
-// note the above parameters now referenced in the 'subject', 'html', and 'text' sections
-// make the to email be your own address or where ever you would like the contact form info sent
-$params = array(
-    'api_user'  => "$user",
-    'api_key'   => "$pass",
-    'to'        => "info@bennyhotellagos.com", // set TO address to have the contact form's email content sent to
-    'subject'   => "Contact Form Submission", // Either give a subject for each submission, or set to $subject
-    'html'      => "<html><head><title> Contact Form</title><body>
-    Name: $name\n<br>
-    Email: $email\n<br>
-    Message: $message <body></title></head></html>", // Set HTML here.  Will still need to make sure to reference post data names
-    'text'      => "
-    Name: $name\n
-    Email: $email\n
-    $message",
-    'from'      => "no-reply@bennyhotellagos.com", // set from address here, it can really be anything
-  );
-curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-$request =  $url.'api/mail.send.json';
-// Generate curl request
-$session = curl_init($request);
-// Tell curl to use HTTP POST
-curl_setopt ($session, CURLOPT_POST, true);
-// Tell curl that this is the body of the POST
-curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-// Tell curl not to return headers, but do return the response
-curl_setopt($session, CURLOPT_HEADER, false);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-// obtain response
-$response = curl_exec($session);
-curl_close($session);
-// Redirect to thank you page upon successfull completion, will want to build one if you don't alreday have one available
-header('Location: /thankyou2.html'); // feel free to use whatever title you wish for thank you landing page, but will need to reference that file name in place of the present 'thanks.html'
-exit();
-// print everything out
-// print_r($response);
- -->
+    header("Location: /thankyou2.html"); -->
