db1502a62aaa4429c470392b566d81132ec5f32d
Chinedu Abalogu <chineduabalogu@yahoo.com>
2016-06-11 11:56:44 +0100

    send mail

--- mail.php
+++ mail.php
@@ -1,18 +1,32 @@
 <?php
 require 'vendor/autoload.php';
-use Mailgun\Mailgun;
 
-# Instantiate the client.
-$client = new \Http\Adapter\Guzzle6\Client(); 
-$mailgun = new \Mailgun\Mailgun('key-e87bf6f1a276e57672a6cacabf72c97a', $client);
-$domain = "sandbox173725272456427b80374db0775c1d92.mailgun.org";
+$mail = new PHPMailer;
 
-# Make the call to the client.
-$mailgun->sendMessage($domain, array(
-    'from'    => 'Excited User <mailgun@YOUR_DOMAIN_NAME>',
-    'to'      => 'Baz <info@bennyhotellagos.com>',
-    'subject' => 'Hello',
-    'text'    => 'Testing some Mailgun awesomness!'
-));
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mailgun.org';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'postmaster@sandbox173725272456427b80374db0775c1d92.mailgun.org';                 // SMTP username
+$mail->Password = '0280a862b6e94a03ff04d30b66adbee0';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->setFrom('postmaster@sandbox173725272456427b80374db0775c1d92.mailgun.org', 'Mailer');
+$mail->addAddress('info@bennyhotellagos.com', 'Joe User');     // Add a recipient
+$mail->addReplyTo('info@example.com', 'Information');
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
 
 ?>
